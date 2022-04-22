23d85f7f0e88afdffd8ff00919b50893071fe023
Fonnos <100319954+Fonnos@users.noreply.github.com>
2022-04-22 22:38:05 +0200

    added first JSON-arrey called Bundelsänder

---
+++ script.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        "question": "Wie viele Bundesländer hat Deutscchland?",
+        "answer_1": "12",
+        "answer_2": "14",
+        "answer_3": "16",
+        "answer_4": "18",
+        "right_answer": "3",
+    },
+    {
+        "question": "Welches Bundesland ist nach Fläche das größte?",
+        "answer_1": "Baden-Württemberg",
+        "answer_2": "Bayern",
+        "answer_3": "Niedersachsen",
+        "answer_4": "Nordrhein-Westfalen",
+        "right_answer": "2",
+    },
+    {
+        "question": "Welches Bundesland zählt die größte Bevölkerung?",
+        "answer_1": "Nordrhein-Westfalen",
+        "answer_2": "Niedersachsen",
+        "answer_3": "Bayern",
+        "answer_4": "Baden-Württemberg",
+        "right_answer": "1",
+    },
+    {
+        "question": "Welches Bundesland hat die höchste Arbeitslosenquote?",
+        "answer_1": "Berlin",
+        "answer_2": "Bremen",
+        "answer_3": "Mecklenburg-Vorpommern",
+        "answer_4": "Nordrhein-Westfalen",
+        "right_answer ":"2",
+    },
+    {
+        "question": "Welches ist das jüngste der alten Bundesländern?",
+        "answer_1": "Schleswig-Holstein",
+        "answer_2": "Hessen",
+        "answer_3": "Brandenburg",
+        "answer_4": "Saarland",
+        "right_answer ":"4",
+    },
+];
