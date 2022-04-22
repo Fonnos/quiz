ef6cb02e395f36e500f8a6101b691075cac97a8c
Fonnos <100319954+Fonnos@users.noreply.github.com>
2022-04-23 01:04:32 +0200

    JS for render Questions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let questions = [
+let questionsFederalStates = [
     {
         "question": "Wie viele Bundesländer hat Deutscchland?",
         "answer_1": "12",
@@ -40,3 +40,91 @@ let questions = [
         "right_answer ":"4",
     },
 ];
+
+let i = 0;
+
+
+function nextQuestion() {
+    i++;
+    renderFederalStates();
+}
+
+
+
+function renderGeographie() {
+    let image = document.getElementById("mainImg")
+    let title = document.getElementById("title");
+    let content = document.getElementById("mainContent");
+    image.classList.add(`p-16`);
+
+    image.src = `
+    img/geographie.png
+    `;
+
+    title.innerHTML = `
+    Wähle ein Thema aus!
+    `;
+    
+    content.innerHTML = `
+    <button onclick="renderFederalStates()" type="button" class="m-16 btn btn-secondary btn-lg">Bundesländer</button>
+    `;
+}
+
+function renderFederalStates() {
+    let image = document.getElementById("mainImg")
+    let title = document.getElementById("title");
+    let quest = document.getElementById("question");
+    let content = document.getElementById("mainContent");
+    let footer = document.getElementById("cardFooter");
+    let maxQuest = questionsFederalStates.length;
+    let question = questionsFederalStates[i];
+    let currentQuest = i + 1;
+    
+
+    image.src = `
+    img/federalStates.png
+    `;
+
+    title.innerHTML = `
+    Bundesländer
+    `;
+
+    quest.innerHTML = `
+    ${question['question']}
+    `;
+    
+    content.innerHTML = `
+    <div class="card">
+        <div class="card-body">
+            ${question['answer_1']}
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-body">
+            ${question['answer_2']}
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-body">
+            ${question['answer_3']}
+        </div>
+    </div>
+
+    <div class="card">
+        <div class="card-body">
+            ${question['answer_4']}
+        </div>
+    </div>
+    `;
+
+    footer.innerHTML = `
+    <div>
+        <b>${currentQuest}</b> von <b>${maxQuest}</b> Fragen
+    </div>
+    <button onclick="nextQuestion()" type="button" class="btn btn-secondary">nächste Frage</button>
+    `;
+
+    
+}
