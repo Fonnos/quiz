79dd492bcbb15b5504736e6b0c4564a0810fd6d4
Fonnos <100319954+Fonnos@users.noreply.github.com>
2022-04-26 23:59:49 +0200

    add more quiz & cleancoding

--- script.js
+++ script.js
@@ -4,12 +4,13 @@ let questionsfederalStates = [
     {
         "title": "Bundesländer",
         "image": "img/federalStates.png",
-        "question": "Wie viele Bundesländer hat Deutscchland?",
+        "question": "Wie viele Bundesländer hat Deutschland?",
         "answer_1": "12",
         "answer_2": "14",
         "answer_3": "16",
         "answer_4": "18",
         "right_answer": "3",
+        "explain": "Wer zählen kann ist klar im Vorteil."
     },
     {
         "title": "Bundesländer",
@@ -20,6 +21,7 @@ let questionsfederalStates = [
         "answer_3": "Niedersachsen",
         "answer_4": "Nordrhein-Westfalen",
         "right_answer": "2",
+        "explain": "Wenn du Augen im Kopf hast, war das auch nicht so schwer, weiter gehts."
     },
     {
         "title": "Bundesländer",
@@ -30,6 +32,7 @@ let questionsfederalStates = [
         "answer_3": "Bayern",
         "answer_4": "Baden-Württemberg",
         "right_answer": "1",
+        "explain": "Mit ca. 18 Mio Einwohnern hat NRW knapp 5 Mio mehr als Bayern."
     },
     {
         "title": "Bundesländer",
@@ -40,6 +43,7 @@ let questionsfederalStates = [
         "answer_3": "Mecklenburg-Vorpommern",
         "answer_4": "Nordrhein-Westfalen",
         "right_answer": "2",
+        "explain": "Jeder 10. Bremer wartet auf sein Schiff und ist dadurch Arbeitslos"
     },
     {
         "title": "Bundesländer",
@@ -50,141 +54,499 @@ let questionsfederalStates = [
         "answer_3": "Brandenburg",
         "answer_4": "Saarland",
         "right_answer": "4",
+        "explain": "Erst 1957 durfte das Saarland in der BRD mitspielen."
     },
 ];
+
+let questionsgermany = [
+    {
+        "title": "Deutschland",
+        "image": "img/germany.jpg",
+        "question": "Welche ist die Hauptstadt der BRD",
+        "answer_1": "Bern",
+        "answer_2": "Berlin",
+        "answer_3": "Bremen",
+        "answer_4": "Bonn",
+        "right_answer": "2",
+        "explain": "Bonn war 40 Jahre lang die Hauptstadt, ab 1991 ist es jedoch Berlin.",
+    },
+    {
+        "title": "Deutschland",
+        "image": "img/germany.jpg",
+        "question": "Wie viele Bürger leben in Deutschland?",
+        "answer_1": "19 Mio",
+        "answer_2": "38 Mio",
+        "answer_3": "60 Mio",
+        "answer_4": "83 Mio",
+        "right_answer": "4",
+        "explain": "Das sind schon viele Menschen.",
+    },
+    {
+        "title": "Deutschland",
+        "image": "img/germany.jpg",
+        "question": "Welches ist der höchste Berg Deutschlands?",
+        "answer_1": "Zugspitze",
+        "answer_2": "Brocken",
+        "answer_3": "Inselberg",
+        "answer_4": "Dolmar",
+        "right_answer": "1",
+        "explain": "Fast 3000 m hoch!Leider müssen wir die Zugspitze mit Österreich teilen.",
+    },
+    {
+        "title": "Deutschland",
+        "image": "img/germany.jpg",
+        "question": "Welches ist die gröste Insel Deutschlands?",
+        "answer_1": "Rügen",
+        "answer_2": "Usedom",
+        "answer_3": "Fehmarn",
+        "answer_4": "Sylt",
+        "right_answer": "1",
+        "explain": "Mit 926km² fast 3x so groß wie Usedom.",
+    },
+    {
+        "title": "Deutschland",
+        "image": "img/germany.jpg",
+        "question": "Der Rhein ist auf deutschen Boden der längste Fluss. Welcher ist der zweitlängste?",
+        "answer_1": "Donau",
+        "answer_2": "Elbe",
+        "answer_3": "Weser",
+        "answer_4": "Main",
+        "right_answer": "3",
+        "explain": "Tatsächlich! Mit seinem Hauptquellfluss, der Werra kommt die Weser auf beachtliche 744km.",
+    },
+];
+
+
+let questionsLotR = [
+    {
+        "title": "Der Herr der Ringe",
+        "image": "img/lotr.png",
+        "question": "Wer schrieb Der Herr der Ringe?",
+        "answer_1": "Robert E. Howard",
+        "answer_2": "Raymond E. Feist",
+        "answer_3": "J.R.R. Tolkien",
+        "answer_4": "Robert Asprin",
+        "right_answer": "3",
+        "explain": "Das sollte einfach gewesen sein.",
+    },
+    {
+        "title": "Der Herr der Ringe",
+        "image": "img/lotr.png",
+        "question": "Wer waren die Orks ursprünglich??",
+        "answer_1": "Elben",
+        "answer_2": "Menschen",
+        "answer_3": "Zwerge",
+        "answer_4": "Trolle",
+        "right_answer": "1",
+        "explain": "Garstige Orks!",
+    },
+    {
+        "title": "Der Herr der Ringe",
+        "image": "img/lotr.png",
+        "question": "Was lieben die Hobbits am meisten?",
+        "answer_1": "Bier",
+        "answer_2": "Regen",
+        "answer_3": "Pflanzen",
+        "answer_4": "Kaninchen",
+        "right_answer": "3",
+        "explain": "Sie mögen zwar auch Bier, aber noch lieber mögen sie Pflanzen.",
+    },
+    {
+        "title": "Der Herr der Ringe",
+        "image": "img/lotr.png",
+        "question": "Welche Stadt wird die weiße Stadt genannt?",
+        "answer_1": "Osgiliath",
+        "answer_2": "Minas Tirith",
+        "answer_3": "Edoras",
+        "answer_4": "Minas Morgul",
+        "right_answer": "2",
+        "explain": "Ein hoch auf den König!",
+    },
+    {
+        "title": "Der Herr der Ringe",
+        "image": "img/lotr.png",
+        "question": "Wie heißt der Sohn von König Théoden?",
+        "answer_1": "Aragon",
+        "answer_2": "Éomer",
+        "answer_3": "Haldir",
+        "answer_4": "Théodred",
+        "right_answer": "4",
+        "explain": "Anführer der Rohirrim!",
+    },
+]
+
+
+let questionsWoW = [
+    {
+        "title": "World of Warcraft",
+        "image": "img/wow.jpg",
+        "question": "Wann startete das originale World of Warcraft in Deutschland?",
+        "answer_1": "November 2004",
+        "answer_2": "Februar 2005",
+        "answer_3": "April 2006",
+        "answer_4": "Oktober 2003",
+        "right_answer": "2",
+        "explain": "So lange ist das schon her!.",
+    },
+    {
+        "title": "World of Warcraft",
+        "image": "img/wow.jpg",
+        "question": "Welche Klasse könnt ihr in Classic nur bei der Horde wählen?",
+        "answer_1": "Todesritter",
+        "answer_2": "Mönch",
+        "answer_3": "Schamane",
+        "answer_4": "Jäger",
+        "right_answer": "3",
+        "explain": "Für die Horde!",
+    },
+    {
+        "title": "World of Warcraft",
+        "image": "img/wow.jpg",
+        "question": "Wer wurde nach Arthas der nächste Lich-König?",
+        "answer_1": "Tirion Fordring",
+        "answer_2": "Ner'Zhul",
+        "answer_3": "Bolvar Fordragon",
+        "answer_4": "Anub'arak",
+        "right_answer": "3",
+        "explain": "Es muss immer einen Lichkönig geben!",
+    },
+    {
+        "title": "World of Warcraft",
+        "image": "img/wow.jpg",
+        "question": "Wie viele Phasen gibt es während des Kampfes mit Onyxia?",
+        "answer_1": "5",
+        "answer_2": "3",
+        "answer_3": "4",
+        "answer_4": "6",
+        "right_answer": "2",
+        "explain": "Ein hoch auf den König!",
+    },
+    {
+        "title": "World of Warcraft",
+        "image": "img/wow.jpg",
+        "question": "Mit welcher Erweiterung wurden die beliebten Haustierkämpfe hinzugefügt?",
+        "answer_1": "Cataclysm",
+        "answer_2": "Legion",
+        "answer_3": "Warlords of Draenor",
+        "answer_4": "Mists of Pandaria",
+        "right_answer": "4",
+        "explain": "Anführer der Rohirrim!",
+    },
+]
+
 
 currentQuestions = [];
 currentArray = 0;
 currentQuest = 1;
 buttonNext = "nächste Frage";
+rightAnswers = 0;
+AUDIO_SUCCESS = new Audio('audio/success.mp3');
+AUDIO_FAIL = new Audio('audio/fail.mp3');
+AUDIO_END = new Audio('audio/endGame.mp3');
 
+
+
+function resetForMainMenu() {
+    document.getElementById("progress-bar").classList.add(`hide`);
+    rightAnswers = 0;
+    document.getElementById('explain').innerHTML = ``;
+    home.classList.add(`hide`);
+    let footer = document.getElementById("cardFooter");
+    footer.innerHTML = ``;
+}
 
 
 function renderMainMenu() {
-    let footer = document.getElementById("cardFooter");
+    resetForMainMenu();
     let image = document.getElementById("mainImg");
     let content = document.getElementById("mainContent");
-    let home = document.getElementById("home")
     let headline = document.getElementById("headline");
-    home.classList.add(`hide`);
-    footer.innerHTML = ``;
     headline.innerHTML = `Quizapp`;
     image.classList.remove(`p-16`);
-    image.src = `
-    img/quiz-time.png
-    `;
-    title.innerHTML = `
-    Wähle eine Kategorie aus!
-    `;
+    image.src = `img/quiz-time.png`;
+    title.innerHTML = `Wähle eine Kategorie aus!`;
     content.innerHTML = `
     <button onclick="renderGeographie()" type="button" class="m-16 btn btn-secondary btn-lg">Geografie</button>
-    <button onclick="" type="button" class="m-16 btn btn-secondary btn-lg">nicht Definiert</button>
+    <button onclick="renderFantasy()" type="button" class="m-16 btn btn-secondary btn-lg">Fantasy</button>
     `;
-
 }
-    
+
+
 function renderGeographie() {
-    let footer = document.getElementById("cardFooter");
-    let image = document.getElementById("mainImg");
+    resetCounter();
+    GeographieHeadline();
+    GeographieImg();
     let content = document.getElementById("mainContent");
-    let home = document.getElementById("home")
+    title.innerHTML = `Wähle ein Thema aus!`;
+    content.innerHTML = `
+    <button onclick="renderCurrentQuestions(questionsfederalStates)" type="button" class="m-16 btn btn-secondary btn-lg">Bundesländer</button>
+    <button onclick="renderCurrentQuestions(questionsgermany)" type="button" class="m-16 btn btn-secondary btn-lg">Deutschland</button>
+    `;
+}
+
+
+function GeographieHeadline() {
     let headline = document.getElementById("headline");
-    footer.innerHTML = ``;
-    home.classList.remove(`hide`);
     headline.innerHTML = `Geografie`;
+    let home = document.getElementById("home")
+    home.classList.remove(`hide`);
     home.onclick=function() {
         renderMainMenu();
     };
+}
+
+
+function GeographieImg() {
+    let image = document.getElementById("mainImg");
     image.classList.add(`p-16`);
-    image.src = `
-    img/geographie.png
-    `;
-    title.innerHTML = `
-    Wähle ein Thema aus!
-    `;
+    image.src = `img/geographie.png`;
+}
+
+
+function renderFantasy() {
+    resetCounter();
+    fantasyHeadline();
+    fantasyImg();
+    let content = document.getElementById("mainContent");
+    title.innerHTML = `Wähle ein Thema aus!`;
     content.innerHTML = `
-    <button onclick="renderCurrentQuestions(questionsfederalStates)" type="button" class="m-16 btn btn-secondary btn-lg">Bundesländer</button>
-    <button onclick="renderQuestions(questionstest)" type="button" class="m-16 btn btn-secondary btn-lg">test</button>
+    <button onclick="renderCurrentQuestions(questionsLotR)" type="button" class="m-16 btn btn-secondary btn-lg">Der Herr der Ringe</button>
+    <button onclick="renderCurrentQuestions(questionsWoW)" type="button" class="m-16 btn btn-secondary btn-lg">World of Warcraft</button>
     `;
 }
+
+
+function fantasyHeadline() {
+    let headline = document.getElementById("headline");
+    headline.innerHTML = `Fantasy`;
+    let home = document.getElementById("home")
+    home.classList.remove(`hide`);
+    home.onclick=function() {
+        renderMainMenu();
+    };
+}
+
+
+function fantasyImg() {
+    let image = document.getElementById("mainImg");
+    image.classList.add(`p-16`);
+    image.src = `img/fantasy.jpg`;
+}
+
 
 function renderCurrentQuestions(array) {
     currentQuestions = array;
+    document.getElementById("progress-bar").classList.remove(`hide`);
+    document.getElementById("progress-bar").innerHTML = `0%`;
+    document.getElementById("progress-bar").style = `width: 0%`;
     renderQuestions();
+    
+    
 }
 
+
 function resetCounter() {
+    let footer = document.getElementById("cardFooter");
+    footer.innerHTML = ``;
     currentQuestions = [];
     currentArray = 0;
     currentQuest = 1;
     buttonNext = "nächste Frage";
 }
 
+
 function nextQuestion() {
-    currentArray++;
-    currentQuest++;
-    if (currentQuest > maxQuest) {
-        renderGeographie();
-        resetCounter();
+    addCounter();
+    updateProgressbar();
+    if (penultimateQuestion()) {
+        showEndScreen(rightAnswers, maxQuest);
     }
-    if (currentQuest == maxQuest) {
+    if (lastQuestion()) {
         buttonNext = "Quiz beenden";
     }
     renderQuestions();
     }
 
-function checkAnswer(o) {
-    console.log(o);
+
+function addCounter() {
+    currentArray++;
+    currentQuest++;
 }
 
-function renderQuestions() {
-    let title = document.getElementById("title");
-    let image = document.getElementById("mainImg");
-    let quest = document.getElementById("question");
-    let content = document.getElementById("mainContent");
-    let footer = document.getElementById("cardFooter");
-    let home = document.getElementById("home")
+function updateProgressbar() {
+    percent = currentArray / maxQuest;
+    percent = percent * 100;
+    document.getElementById("progress-bar").innerHTML = `${percent}%`;
+    document.getElementById("progress-bar").style = `width: ${percent}%`;
+}
+
+function penultimateQuestion() {
+    return currentQuest > maxQuest;
+}
+
+function lastQuestion() {
+    return currentQuest == maxQuest;
+}
+
+
+function checkAnswer(selection) {
+    let rightAnswer = currentQuestions[currentArray].right_answer;
+    selectedNumber = selection.slice(-1);
+    let idOfRightAnswer = `answer_${rightAnswer}`;
+    if (numberIsRight(rightAnswer, selectedNumber)) {
+        showRight(selection, rightAnswers);
+        rightAnswers++;
+        }
+    else {
+        showFail(selection, idOfRightAnswer);
+        }
+    document.getElementById("next-button").disabled = false;
+    document.getElementById(idOfRightAnswer).parentElement.onclick = '';
+}
+
+
+function numberIsRight(rightAnswer, selectedNumber) {
+    return selectedNumber == rightAnswer
+}
+
+
+function showRight(selection) {
     let question = currentQuestions[currentArray];
+    let explaination = question['explain'];
+    document.getElementById(selection).parentNode.classList.add('bg-success');
+    document.getElementById("explain").innerHTML = `Das ist richtig! ${explaination}`;
+    AUDIO_SUCCESS.play();
+}
+
+
+function showFail(selection, idOfRightAnswer) {
+    let question = currentQuestions[currentArray];
+    let explaination = question['explain'];
+    document.getElementById(selection).parentNode.classList.add('bg-danger');
+    document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+    document.getElementById("explain").innerHTML = `Das ist falsch! ${explaination}`;
+    AUDIO_FAIL.play();
+}
+
+
+function showEndScreen() {
+    AUDIO_END.play();
+    resetForEndscreen();
+    let image = document.getElementById("mainImg");
+    let headline = document.getElementById("headline");
+    headline.innerHTML = `Glückwunsch!`;
+    image.src = `img/endScreen.jpg`;
+    title.innerHTML = `
+    Du hast das Quiz beendet!
+    Du hast ${rightAnswers} von ${maxQuest} richtig beantwortet!
+    <button onclick="renderMainMenu()" type="button" class="m-16 btn btn-primary">Hauptmenü</button>
+    `;
+    }
+
+
+function resetForEndscreen() {
+    document.getElementById("explain").innerHTML = ``;
+    document.getElementById("cardFooter").innerHTML = ``;
+    let content = document.getElementById("mainContent");
+    content.innerHTML = ``;
+}
+
+
+function renderQuestions() {
+    let question = currentQuestions[currentArray];
+    resetForQuestions();
+    headlineQuestions(question);
+    imgQuestions(question);
+    contentQuestions(question);
+    footerQuestions();
+    }
+
+
+function resetForQuestions() {
+    document.getElementById("explain").innerHTML = ``;
+    document.getElementById("mainContent").innerHTML = ``;
+
+
+}
+
+
+function imgQuestions(question) {
+    let image = document.getElementById("mainImg");
+    image.src = `${question['image']}`;
+}
+
+
+function headlineQuestions(question) {
     let headline = document.getElementById("headline");
     headline.innerHTML = question['title'];
-    maxQuest = currentQuestions.length;
-    
-    image.src = `
-        ${question['image']}
-    `;
-    title.innerHTML = `
-        ${question['title']}
-    `;
-    quest.innerHTML = `
-        ${question['question']}
-    `;
-    content.innerHTML = `
-        <div class="card answer" onclick="checkAnswer(1)">
-            <div class="card-body">
+}
+
+
+function answer_1(question) {
+    let content = document.getElementById("mainContent");
+    content.innerHTML += `
+        <div class="card answer" onclick="checkAnswer('answer_1')">
+            <div id="answer_1" class="card-body">
                 ${question['answer_1']}
             </div>
         </div>
-        <div class="card answer" onclick="checkAnswer(2)">
-            <div class="card-body">
+        `;
+}
+
+function answer_2(question) {
+    let content = document.getElementById("mainContent");
+    content.innerHTML += `
+        <div class="card answer" onclick="checkAnswer('answer_2')">
+            <div id="answer_2" class="card-body">
                 ${question['answer_2']}
             </div>
         </div>
-        <div class="card answer" onclick="checkAnswer(3)">
-            <div class="card-body">
+        `;
+}
+
+function answer_3(question) {
+    let content = document.getElementById("mainContent");
+    content.innerHTML += `
+        <div class="card answer" onclick="checkAnswer('answer_3')">
+            <div id="answer_3" class="card-body">
                 ${question['answer_3']}
             </div>
         </div>
-        <div class="card answer" onclick="checkAnswer(4)">
-            <div class="card-body">
+        `;
+}
+
+function answer_4(question) {
+    let content = document.getElementById("mainContent");
+    content.innerHTML += `
+        <div class="card answer" onclick="checkAnswer('answer_4')">
+            <div id="answer_4" class="card-body">
                 ${question['answer_4']}
             </div>
         </div>
-    `;
+        `;
+}
+
+
+function contentQuestions(question) {
+    let quest = document.getElementById("title");
+    maxQuest = currentQuestions.length;
+    quest.innerHTML = `${question['question']}`;
+    answer_1(question);
+    answer_2(question);
+    answer_3(question);
+    answer_4(question);
+}
+
+function footerQuestions() {
+    let footer = document.getElementById("cardFooter");
     footer.innerHTML = `
         <div>
             <b>${currentQuest}</b> von <b>${maxQuest}</b> Fragen
         </div>
-        <button onclick="nextQuestion()" class="btn btn-secondary">${buttonNext}</button>
+        <button id="next-button" onclick="nextQuestion()" class="btn btn-secondary" disabled>${buttonNext}</button>
     `;
 }
+
+
+
 
